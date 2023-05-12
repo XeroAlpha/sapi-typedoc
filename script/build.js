dee16a0fa0dbbbdaeb7ab6e402e18846022041f4
ProjectXero <projectxero@163.com>
2023-05-12 12:30:07 +0800

    手动解析内联链接目标

--- script/build.js
+++ script/build.js
@@ -15,10 +15,36 @@ async function build() {
     });
     const project = app.convert();
     if (project) {
-        Object.entries(project.reflections).forEach(([id, reflection]) => {
+        const reflectionEntries = Object.entries(project.reflections);
+        const visitCommentPart = (/** @type {TypeDoc.CommentDisplayPart} */part) => {
+            if (part.kind === 'inline-tag' && part.tag === '@link') {
+                if (typeof part.target === 'string') {
+                    return;
+                }
+                if (typeof part.target === 'object' && part.target.name === part.text) {
+                    return;
+                }
+                const lastSegment = part.text.split(/[\./]/).pop();
+                const foundReflections = reflectionEntries.filter(([_, refl]) => refl.name === lastSegment);
+                if (foundReflections.length === 0) {
+                    return;
+                }
+                if (foundReflections.length >= 2) {
+                    console.warn(`Multiple resolutions of link: ${part.text}`);
+                }
+                part.target = foundReflections[0][1];
+            }
+        }
+        reflectionEntries.forEach(([id, reflection]) => {
             if (reflection.sources) {
                 reflection.sources.forEach((source) => {
                     source.fileName = source.fileName.replace("translated", project.name);
+                });
+            }
+            if (reflection.comment) {
+                reflection.comment.summary.forEach(visitCommentPart);
+                reflection.comment.blockTags.forEach((tag) => {
+                    tag.content.forEach(visitCommentPart);
                 });
             }
         });
