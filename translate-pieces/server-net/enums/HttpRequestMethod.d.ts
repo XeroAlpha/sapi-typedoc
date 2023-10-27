8fca98fdd4da9ddd161fb49e7e8a96b685bf27b6
ProjectXero <projectxero@163.com>
2023-10-27 21:12:01 +0800

    HttpRequestMethod
    /translated server-net/variables/http

--- translate-pieces/server-net/enums/HttpRequestMethod.d.ts
+++ translate-pieces/server-net/enums/HttpRequestMethod.d.ts
@@ -1,6 +1,9 @@
 export enum HttpRequestMethod {
     /**
      * @remarks
+     * 表示 HTTP DELETE 请求。
+     * DELETE 通常用于描述删除指定的资源。
+     * 
      * Represents the method for an HTTP HEAD request. HEAD
      * requests are similar to a GET request, but are commonly used
      * to retrieve just the HTTP response headers from the
@@ -10,6 +13,9 @@ export enum HttpRequestMethod {
     Delete = 'Delete',
     /**
      * @remarks
+     * 表示 HTTP GET 请求。
+     * GET 请求通常用于获取指定资源的信息。
+     * 
      * Represents the method for an HTTP PUT request. POST requests
      * are commonly used to create a new resource that is a
      * subordinate of the specified URI.
@@ -18,6 +24,9 @@ export enum HttpRequestMethod {
     Get = 'Get',
     /**
      * @remarks
+     * 表示 HTTP HEAD 请求。
+     * HEAD 请求类似于 GET 请求，但通常用于仅需要获取指定资源的 HTTP 响应标头，不需要获取响应主体的情况。
+     * 
      * Represents the method for an HTTP PUT request. GET requests
      * are commonly used to retrieve information about a resource
      * at the specified URI.
@@ -26,6 +35,9 @@ export enum HttpRequestMethod {
     Head = 'Head',
     /**
      * @remarks
+     * 表示 HTTP POST 请求。
+     * POST 请求通常用于在指定 URI 下创建新资源。
+     * 
      * Represents the method for an HTTP PUT request. GET requests
      * are commonly used to retrieve information about a resource
      * at the specified URI.
@@ -34,6 +46,9 @@ export enum HttpRequestMethod {
     Post = 'Post',
     /**
      * @remarks
+     * 表示 HTTP PUT 请求。
+     * PUT 请求通常用于更新某个已存在于资源集合中的资源。
+     * 
      * Represents the method for an HTTP PUT request. PUT requests
      * are commonly used to update a single resource that already
      * exists in a resource collection.
