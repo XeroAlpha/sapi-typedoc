2b927ba55c65beb48ecd2bc4e7803830a92e5765
rice_awa <q77884590@163.com>
2025-02-04 22:37:28 +0800

    修正

--- translate-pieces/server/enums/StructureAnimationMode.d.ts
+++ translate-pieces/server/enums/StructureAnimationMode.d.ts
@@ -1,7 +1,7 @@
 /**
  * Specifies how structure blocks should be animated when a
  * structure is placed.
- * 
+ *
  * 表示在放置结构时结构方块的使用的动画。
  */
 export enum StructureAnimationMode {
@@ -10,7 +10,7 @@ export enum StructureAnimationMode {
      * Blocks will be randomly placed one at at time. Use
      * @minecraft/server.StructurePlaceOptions.animationSeconds to
      * control how long it takes for all blocks to be placed.
-     * 
+     *
      * 表示方块将会被随机一个一个地放置。使用 @minecraft/server.StructurePlaceOptions.animationSeconds 来控制所有方块放置完成所需的时间。
      */
     Blocks = 'Blocks',
@@ -20,14 +20,14 @@ export enum StructureAnimationMode {
      * top. Use
      * @minecraft/server.StructurePlaceOptions.animationSeconds to
      * control how long it takes for all blocks to be placed.
-     * 
+     *
      * 表示方块将会自下而上一层一层地放置。使用 @minecraft/server.StructurePlaceOptions.animationSeconds 来控制所有方块放置完成所需的时间。
      */
     Layers = 'Layers',
     /**
      * @remarks
      * All blocks will be placed immediately.
-     * 
+     *
      * 表示所有方块将会立即放置。
      */
     None = 'None',
