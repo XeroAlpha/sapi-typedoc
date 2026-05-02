c1228b344ec5d1f433a6db11e0f3a3f26a7da467
ForceTen123 <245104466+ForceTen123@users.noreply.github.com>
2026-05-02 22:53:16 +0800

    WIP MessageFormData

--- translate-pieces/server-ui/classes/MessageFormData.d.ts
+++ translate-pieces/server-ui/classes/MessageFormData.d.ts
@@ -3,6 +3,8 @@
 /* IMPORT */ import { MessageFormResponse } from '..';
 
 /**
+ * 构建简易的双按钮模态对话框。
+ *
  * Builds a simple two-button modal dialog.
  * @seeExample showBasicMessageForm.ts
  * @seeExample showTranslatedMessageForm.ts
@@ -10,26 +12,40 @@
 export class MessageFormData {
     /**
      * @remarks
+     * 设定模态表单的主体文本。
+     *
      * Method that sets the body text for the modal form.
      *
+     * @param bodyText
+     * 主体文本内容。
      */
     body(bodyText: RawMessage | string): MessageFormData;
     /**
      * @remarks
+     * 设定对话框第一个按钮的文本。
+     *
      * Method that sets the text for the first button of the
      * dialog.
      *
+     * @param text
+     * 按钮文本内容。
      */
     button1(text: RawMessage | string): MessageFormData;
     /**
      * @remarks
+     * 设定对话框第二个按钮的文本。
+     *
      * This method sets the text for the second button on the
      * dialog.
      *
+     * @param text
+     * 按钮文本内容。
      */
     button2(text: RawMessage | string): MessageFormData;
     /**
      * @remarks
+     * 创建并展示此模态弹出式表单，玩家点击确定或取消按钮后异步返回。
+     *
      * Creates and shows this modal popup form. Returns
      * asynchronously when the player confirms or cancels the
      * dialog.
@@ -37,8 +53,10 @@ export class MessageFormData {
      * @worldMutation
      *
      * @param player
+     * 要展示对话框的玩家。
+     *
      * Player to show this dialog to.
-     * @throws This function can throw errors.
+     * @throws 此函数可能抛出错误。
      *
      * {@link EngineError}
      *
@@ -49,8 +67,12 @@ export class MessageFormData {
     show(player: Player): Promise<MessageFormResponse>;
     /**
      * @remarks
+     * 设定对话框标题。
+     *
      * This builder method sets the title for the modal dialog.
      *
+     * @param titleText
+     * 标题文本内容。
      */
     title(titleText: RawMessage | string): MessageFormData;
 }
