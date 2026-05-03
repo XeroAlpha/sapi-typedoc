a31b439678fa56747887598959a274007bc46fe8
ForceTen123 <245104466+ForceTen123@users.noreply.github.com>
2026-05-03 19:55:19 +0800

    翻译 Update FormResponse

--- translate-pieces/server-ui/classes/FormResponse.d.ts
+++ translate-pieces/server-ui/classes/FormResponse.d.ts
@@ -1,18 +1,24 @@
 /* IMPORT */ import { FormCancelationReason } from '..';
 
 /**
+ * 表单结果的基础类型。
+ *
  * Base type for a form response.
  */
 export class FormResponse {
     private constructor();
     /**
      * @remarks
+     * 表单被取消的附加原因。
+     *
      * Contains additional details as to why a form was canceled.
      *
      */
     readonly cancelationReason?: FormCancelationReason;
     /**
      * @remarks
+     * 若为 `true`，则表单由玩家取消（例如点击了关闭按钮）。
+     *
      * If true, the form was canceled by the player (e.g., they
      * selected the pop-up X close button).
      *
